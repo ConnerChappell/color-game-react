5df90d405b09c727562a633b9f45dc0320991054
Conner Chappell <conner.chappell123@gmail.com>
2024-07-21 18:55:46 -0600

    initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1 className='text-4xl font-extrabold lg:text-5xl'>Color Game</h1>
+    </>
+  )
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import MillionLint from '@million/lint';
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+export default defineConfig({
+  plugins: [react(), MillionLint.vite()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src")
+    }
+  }
+});
